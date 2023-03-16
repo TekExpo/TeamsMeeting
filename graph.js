9aa58c176749f33d6979d23910f545fba1391f85
TekExpo <jeet.m.jadhav@gmail.com>
2023-03-15 22:18:30 -0400

    Updated Code
    Updated Code

--- graph.js
+++ graph.js
@@ -16,3 +16,48 @@ async function getUser() {
         .get();
 }
 
+
+async function getMeeting() {
+    
+    const onlineMeeting = {
+      startDateTime: '2019-07-12T14:30:34.2444915-07:00',
+      endDateTime: '2019-07-12T15:00:34.2464912-07:00',
+      subject: 'User meeting in Microsoft Teams channel.',
+      joinMeetingIdSettings: {
+        isPasscodeRequired: false
+      }
+    };
+    
+    return await graphClient.api('/me/onlineMeetings')
+        .post(onlineMeeting);
+
+
+
+
+   // const graphClient = MicrosoftGraph.Client.initWithMiddleware({ authProvider });
+    var meetingDetails = {
+        startDateTime: new Date().toISOString(),
+        endDateTime: new Date(Date.now() + 60 * 60 * 1000).toISOString(),
+        subject: "Teams Meeting",
+        attendees: [
+            {
+                emailAddress: {
+                    address: "attendee1@example.com"
+                }
+            },
+            {
+                emailAddress: {
+                    address: "attendee2@example.com"
+                }
+            }
+        ],
+        isOnlineMeeting: true,
+        onlineMeetingProvider: "teamsForBusiness"
+    };
+    
+    ensureScope('user.read');
+    return await graphClient
+    .api("me/onlineMeetings")
+    .post({body:meetingDetails});
+    
+}
--- ui.js
+++ ui.js
@@ -6,9 +6,21 @@ async function displayUI() {
     var userName = document.getElementById('userName');
     userName.innerText = user.displayName;  
 
+    
+ 
+
     // Hide login button and initial UI
     var signInButton = document.getElementById('signin');
     signInButton.style = "display: none";
     var content = document.getElementById('content');
     content.style = "display: block";
+}
+
+async function displayMeeting() {
+
+ // Display Meeting Link
+ const MeetingLink = await getMeeting();
+ var lnkMeeting = document.getElementById('MeetingLink');
+  var meetingDiv = document.getElementById("meetingUrlContainer");
+  meetingDiv.innerHTML = "<a href='" + MeetingLink.joinWebUrl + "' target='_blank'>" + "Meeting Link to Join" + "</a>";
 }
